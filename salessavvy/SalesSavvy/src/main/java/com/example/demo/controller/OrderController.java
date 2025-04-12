5fbfde40d788cf0009db238a78e95ad95ff1436f
Abhishek Gowda T J <abhigowdatj18@gmail.com>
2025-04-12 12:04:06 +0530

    Update OrderController.java

--- salessavvy/SalesSavvy/src/main/java/com/example/demo/controller/OrderController.java
+++ salessavvy/SalesSavvy/src/main/java/com/example/demo/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true") // Allow cross-origin requests
+@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true") // Allow cross-origin requests
 @RequestMapping("/api/orders")
 public class OrderController {
 
